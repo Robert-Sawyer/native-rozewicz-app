4933dec539ee9cc1bdf532b4c5b9a0661211f62b
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-08-29 12:28:22 +0200

    Dodanie zewnętrznych czcionek

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import Thunk from 'redux-thunk'
+import * as Font from 'expo-font'
+import AppLoading from "expo-app-loading";
 import authReducer from "./store/reducers/auth";
 import AppNavigator from "./navigation/AppNavigator";
 import placesReducer from "./store/reducers/places";
@@ -14,22 +15,33 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, applyMiddleware(Thunk))
 
+const fetchFonts = () => {
+    return (
+        Font.loadAsync({
+            'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+            'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+        })
+    )
+}
+
 export default function App() {
+
+    const [fontsLoaded, setFontsLoaded] = useState(false)
+
+    if (!fontsLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => {
+                    setFontsLoaded(true)
+                }}
+                onError={console.warn}
+            />
+        )
+    }
   return (
-    // <View style={styles.container}>
-    //   <Text>Hello1</Text>
-    // </View>
       <Provider store={store}>
           <AppNavigator/>
       </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
