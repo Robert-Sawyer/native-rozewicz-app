21350dbd888415b0f84fee55e1ce8cf1c4400898
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-09-10 16:05:59 +0200

    Modyfikacja PlaceItem

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -13,10 +13,10 @@ const PlaceItem = ({onSelect, imgHeight, image, title, detailsHeight, actionHeig
         <View style={styles.prodItem}>
             <TouchableComp onPress={onSelect}>
                 <View style={styles.insideContainer}>
-                    <View style={{...styles.imgContainer, ...{height: imgHeight}}}>
+                    <View style={styles.imgContainer}>
                         <Image style={styles.image} source={{uri: image}}/>
                     </View>
-                    <View style={{...styles.details, ...{height: detailsHeight}}}>
+                    <View style={styles.details}>
                         <Text style={styles.title} numberOfLines={1}>{title}</Text>
                     </View>
                     <View style={{...styles.action, ...{height: actionHeight}}}>
@@ -34,17 +34,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     insideContainer: {
+        flexDirection: 'row',
         width: '100%',
         height: '100%',
+        padding: 10,
     },
-    // imgContainer: {
-    //     width: '100%',
-    // }
+    imgContainer: {
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        overflow: 'hidden'
+    },
     details: {
+        marginLeft: 10,
         alignItems: 'center',
+        justifyContent: 'center',
     },
     title: {
-        fontSize: 15,
+        fontSize: 23,
+        letterSpacing: 1.5,
         marginVertical: 3,
     },
     action: {
@@ -52,6 +60,10 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         alignItems: 'center',
     },
+    image:{
+        width: '100%',
+        height: '100%',
+    }
 })
 
 export default PlaceItem
