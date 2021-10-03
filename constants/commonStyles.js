05a6221886e09baebfbb94b3657c899fe808b1f1
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-10-03 17:26:20 +0200

    Dodanie pliku na wspólne i powtarzające się style

---
+++ constants/commonStyles.js
@@ -0,0 +1,28 @@
+import Colors from './colors'
+
+export default {
+    tooltip: {
+        width: 300,
+        minHeight: 150,
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        paddingVertical: 7,
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: Colors.mainColor,
+        borderWidth: 2.5,
+        marginBottom: 5,
+    },
+    tooltipTitle: {
+        fontFamily: 'roundulliard',
+        fontSize: 21,
+        textAlign: 'center',
+        marginBottom: 5,
+    },
+    tooltipDesc: {
+        fontSize: 18,
+        fontFamily: 'roundulliard',
+        textAlign: 'center'
+    },
+}
